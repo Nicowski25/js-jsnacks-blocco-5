efd7cbe7aacf8aec9b301a6c844e2d8201097358
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-24 12:40:25 +0100

    start snack 2

---
+++ snack_2/assets/js/scripts.js
@@ -0,0 +1,8 @@
+//scrivi una funzione che accetti 3 argomenti: un array e due numeri ('a' più piccolo di 'b' e b grande al massimo quanto il numero di elementi dell'array).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra 'a' e 'b'
+
+//creo la funzione che crea i 3 argomenti 
+//faccio creare l'array
+//faccio inserire b che sia minore di array.len
+//faccio inserire a che sia minore di b
+//ritorno un nuovo array con le posizioni comprese tra a e b
