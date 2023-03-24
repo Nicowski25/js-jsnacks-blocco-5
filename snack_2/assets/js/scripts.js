87fb6adfb62e09c22a746f89d0f5e4da8a6ca649
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-24 13:06:54 +0100

    snack 2

--- snack_2/assets/js/scripts.js
+++ snack_2/assets/js/scripts.js
@@ -6,3 +6,27 @@
 //faccio inserire b che sia minore di array.len
 //faccio inserire a che sia minore di b
 //ritorno un nuovo array con le posizioni comprese tra a e b
+
+let arrayProva = ['nico', 'simo', 'fabio', 'luca', 'tizio', 'pippo', 'pluto', 'mario']
+console.log(arrayProva.length);
+
+function arrayBetween(lista) {
+    let b = Number(prompt('inserisci un numero minore di ' + lista.length))
+    while (b > lista.length) {
+        let b = Number(prompt('reinserisci il numero'))
+        console.log(b);
+    }
+    let a = Number(prompt('inserisci un numero minore di ' + b))
+    while (a >= b) {
+        let a = Number(prompt('reinserisci il numero'))
+        console.log(a);
+    }
+    let newLista = [];
+    for (let i = a; i <= b; i++) {
+        newLista.push(i);
+    }
+    console.log(newLista);
+}
+
+console.log(arrayBetween(arrayProva));
+
